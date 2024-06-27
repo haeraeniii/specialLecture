c3b1175df37aba879078675511943322da87226c
haeraenii <jhr@learninvest.co.kr>
2024-06-27 10:39:13 +0900

    init

---
+++ src/test/java/hhplus/specialLecture/SpecialLectureApplicationTests.java
@@ -0,0 +1,13 @@
+package hhplus.specialLecture;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpecialLectureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
